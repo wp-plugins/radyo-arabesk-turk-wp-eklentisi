d6cf55f8705706b39f96f72808dbad33e1f7e81b
arabeskturk <arabeskturk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-29 08:34:32 +0000

    sürüm2
    git-svn-id: https://plugins.svn.wordpress.org/radyo-arabesk-turk-wp-eklentisi/tags/2.0@1034914 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ radyo-html5-player.php
@@ -0,0 +1,66 @@
+<?php
+/*
+Plugin Name: Radyo Arabesk TÜRK HTML5 Player
+Plugin URI: http://www.radyoarabeskturk.com/
+Description: Radyo Arabesk TÜRK HTML5 Radyo Playeri İsterseniz Widget Olarak İsterseniz Kısa Kodu Kullanarak Sayfanıza Yada Yazılarınıza Ekleyebilirsiniz [radyoarabeskturk].
+Author: Radyo Arabesk TÜRK
+Author URI: http://www.radyoarabeskturk.com
+Version: 1.0
+*/
+
+/*-- Create Sortcode Radyo Arabeskturk --*/
+function radyo_arabeskturk_source( $atts ){
+?>
+<iframe height="350" scrolling="no" src="http://www.radyoarabeskturk.com/dinle/" width="100%"></iframe>
+
+<?php
+}
+add_shortcode('radyoarabeskturk', 'radyo_arabeskturk_source');
+add_filter('widget_text', 'do_shortcode');
+
+
+/*-- Widget  Radyoarabeskturk Player --*/
+class radyoarabeskturkwidget extends WP_Widget
+{
+  function radyoarabeskturkwidget()
+  {
+    $widget_ops = array('classname' => 'radyoarabeskturkwidget', 'description' => 'Radyoarabeskturk Player widget.' );
+    $this->WP_Widget('radyoarabeskturkwidget', 'Radyo Arabesk TURK', $widget_ops);
+  }
+ 
+  function form($instance) 
+  {
+    $instance = wp_parse_args( (array) $instance, array( 'title' => '' ) );
+    $title = $instance['title'];
+?>
+  <p><label for="<?php echo $this->get_field_id('title'); ?>">Title: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo attribute_escape($title); ?>" /></label></p>
+<?php
+  }
+ 
+  function update($new_instance, $old_instance)
+  {
+    $instance = $old_instance;
+    $instance['title'] = $new_instance['title'];
+    return $instance;
+  }
+ 
+  function widget($args, $instance)
+  {
+    extract($args, EXTR_SKIP);
+ 
+    //echo $before_widget;
+    $title = empty($instance['title']) ? ' ' : apply_filters('widget_title', $instance['title']);
+ 
+    if (!empty($title))
+    echo $before_title . $title . $after_title;;
+ 
+    // WIDGET CODE GOES HERE
+  ?>
+<iframe height="200" scrolling="no" src="http://www.radyoarabeskturk.com/dinle/" width="100%"></iframe>
+ <?php
+    echo $after_widget;
+  }
+ 
+}
+add_action( 'widgets_init', create_function('', 'return register_widget("radyoarabeskturkwidget");') );
+?>
